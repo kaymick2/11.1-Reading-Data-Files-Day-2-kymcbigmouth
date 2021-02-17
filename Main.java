0844f391ed4bae65d798a5694a2da7a8583a6826
Kyle McDowell <kymcd21@icstudents.org>
2021-02-17 02:32:50 +0000

    THIS IS NOT WORKING

--- Main.java
+++ Main.java
@@ -1,27 +1,34 @@
 import java.io.*;
 import java.util.*;
-
+import java.lang.*;
 import javax.annotation.processing.Filer;
 
 class Main {
   public static void main(String[] args) throws Exception {
-    Scanner console=new Scanner(System.in);
+    Scanner console = new Scanner(System.in);
     System.out.print("Input Filename: ");
-    String inputFileName=console.next();
+    String inputFileName = console.next();
 
-    //let's make da file!!!!!! pog!!!!!
+    // let's make da file!!!!!! pog!!!!!
     File readFile = new File(inputFileName);
-    Scanner fileReadIn= new Scanner(readFile);
+    Scanner fileReadIn = new Scanner(readFile);
 
-    //ableton loops but java
-    int i=1;
-
-    for (int lineNo=1; fileReadIn.hasNextLine(); lineNo++){
+    // ableton loops but java
+    int i = 1;
+    String nomePais = null;
+    String populacaoPais = null;
+    for (int lineNo = 1; fileReadIn.hasNextLine(); lineNo++) {
       String line = fileReadIn.nextLine();
-      while(!Character.isDigit(line.charAt(i))){
+      while (!Character.isDigit(line.charAt(i))) {
+        nomePais = line.substring(0, i);
+        populacaoPais = line.substring(i);
         i++;
-        System.out.println(line);
       }
-    } 
+
+      nomePais = nomePais.trim();
+      populacaoPais = populacaoPais.trim();
+      System.out.println(nomePais);
+      System.out.println(populacaoPais);
+    }
   }
 }
